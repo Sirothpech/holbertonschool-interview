c93919eb4dd052798d324f65a3959f39fc4ed1f9
Sirothpech <christophe.ngan@gmail.com>
2024-01-19 16:32:17 +0100

    Task 1 : Add heap_insert function to insert a value into a Max Binary Heap

--- heap_insert/binary_trees.h
+++ heap_insert/binary_trees.h
@@ -17,9 +17,13 @@ typedef struct binary_tree_s
 	struct binary_tree_s *parent;
 	struct binary_tree_s *left;
 	struct binary_tree_s *right;
-} binary_tree_t;
+} binary_tree_t, heap_t;
 
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+heap_t *heap_insert(heap_t **root, int value);
+heap_t *swap_value(heap_t *parent, heap_t *child);
+int get_size(heap_t *tree);
+heap_t *get_parent(heap_t *tree, int size, int index);
 
 #endif /* _BINARY_TREES_H_ */
