bb2bc3fbf511456231a6615ae9eed12b8771959f
Sirothpech <christophe.ngan@gmail.com>
2024-01-22 16:07:39 +0100

    Refactor minOperations function to improve readability and performance

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
-"""Interview 3: Minimum Operations"""
+"""Minimum Operations"""
 
 
 def minOperations(n):
     """
-    Method that calculates the fewest number of operations
-    needed to result in exactly n H characters in the file.
+    Calculates the fewest number of operations required to
+    obtain exactly n 'H' characters in the file.
+
+    Args:
+        n (int): The target number of 'H' characters.
+
+    Returns:
+        int: The minimum number of operations needed.
     """
 
     if n <= 1:
         return 0
 
-    minOperations = 0
+    operations = 0
 
     while n % 2 == 0:
-        minOperations += 2
-        n /= 2
+        operations += 2
+        n //= 2
 
     i = 3
     while i * i <= n:
         while n % i == 0:
-            minOperations += i
-            n /= i
+            operations += i
+            n //= i
         i += 2
     if n > 2:
-        minOperations += n
+        operations += n
 
-    return int(minOperations)
+    return int(operations)
