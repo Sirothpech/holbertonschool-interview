926cff0d87ab5976cac476abe0a31cc9e7b21ad2
Sirothpech <christophe.ngan@gmail.com>
2024-02-09 11:56:22 +0100

    Add reverse_list function and update is_palindrome function

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -3,41 +3,70 @@
 
 
 /**
+ * reverse_list - reverses a linked list
+ * @head: pointer to the head of the linked list
+ * Return: pointer to the head of the reversed linked list
+ */
+listint_t *reverse_list(listint_t **head)
+{
+    listint_t *prev = NULL, *current = *head, *next;
+
+    while (current != NULL)
+    {
+        next = current->next;
+        current->next = prev;
+        prev = current;
+        current = next;
+    }
+
+    *head = prev;
+    return (*head);
+}
+
+/**
  * is_palindrome - checks if a singly linked list is a palindrome
  * @head: pointer to the head of the linked list
- * Return: 1 if it is a palindrome, 0 otherwise
+ * Return: 1 if the linked list is a palindrome, 0 otherwise
  */
 int is_palindrome(listint_t **head)
 {
-	if (*head == NULL || (*head)->next == NULL)
-		return (1);
+    listint_t *slow = *head, *fast = *head;
+    listint_t *second_half, *prev_slow = *head;
+    int result = 1; /* Assume linked list is a palindrome */
 
-	listint_t *slow = *head;
-	listint_t *fast = *head;
-	listint_t *prev = NULL;
-	listint_t *temp;
+    if (*head == NULL || (*head)->next == NULL)
+        return (result); /* An empty list or single-node list is a palindrome */
 
-	while (fast != NULL && fast->next != NULL)
-	{
-		fast = fast->next->next;
+    /* Find the middle of the linked list */
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        prev_slow = slow;
+        slow = slow->next;
+    }
 
-		temp = slow->next;
-		slow->next = prev;
-		prev = slow;
-		slow = temp;
-	}
+    /* If the number of nodes is odd, move slow pointer one step forward */
+    if (fast != NULL)
+        slow = slow->next;
 
-	if (fast != NULL)
-		slow = slow->next;
+    /* Reverse the second half of the linked list */
+    second_half = reverse_list(&slow);
 
-	while (slow != NULL)
-	{
-		if (slow->n != prev->n)
-			return (0);
+    /* Compare the first half with the reversed second half */
+    while (second_half != NULL)
+    {
+        if ((*head)->n != second_half->n)
+        {
+            result = 0; /* Not a palindrome */
+            break;
+        }
+        *head = (*head)->next;
+        second_half = second_half->next;
+    }
 
-		slow = slow->next;
-		prev = prev->next;
-	}
+    /* Restore the original list */
+    reverse_list(&slow);
+    prev_slow->next = slow;
 
-	return (1);
+    return (result);
 }
