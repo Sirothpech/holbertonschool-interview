10309bd7db2101a323d2a866f46c5487512b614e
Sirothpech <christophe.ngan@gmail.com>
2024-02-09 11:51:25 +0100

    Refactor is_palindrome and linked_lists.c

--- linked_list_palindrome/0-is_palindrome.c
+++ linked_list_palindrome/0-is_palindrome.c
@@ -8,39 +8,35 @@
  */
 int is_palindrome(listint_t **head)
 {
-    if (*head == NULL || (*head)->next == NULL)
-        return 1; // An empty list or a single-node list is a palindrome
+	if (*head == NULL || (*head)->next == NULL)
+		return (1);
 
-    listint_t *slow = *head;
-    listint_t *fast = *head;
-    listint_t *prev = NULL;
-    listint_t *temp;
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL;
+	listint_t *temp;
 
-    // Find the middle of the list using the slow and fast pointers
-    while (fast != NULL && fast->next != NULL)
-    {
-        fast = fast->next->next;
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
 
-        // Reverse the first half of the list
-        temp = slow->next;
-        slow->next = prev;
-        prev = slow;
-        slow = temp;
-    }
+		temp = slow->next;
+		slow->next = prev;
+		prev = slow;
+		slow = temp;
+	}
 
-    // If the number of nodes is odd, move the slow pointer one step forward
-    if (fast != NULL)
-        slow = slow->next;
+	if (fast != NULL)
+		slow = slow->next;
 
-    // Compare the reversed first half with the second half
-    while (slow != NULL)
-    {
-        if (slow->n != prev->n)
-            return 0; // Not a palindrome
+	while (slow != NULL)
+	{
+		if (slow->n != prev->n)
+			return (0);
 
-        slow = slow->next;
-        prev = prev->next;
-    }
+		slow = slow->next;
+		prev = prev->next;
+	}
 
-    return (1); // It is a palindrome
+	return (1);
 }
