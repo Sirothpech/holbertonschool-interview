4397d62967e18fe1b4e553c98cc5c4f84b212d92
Sirothpech <christophe.ngan@gmail.com>
2024-02-13 14:29:38 +0100

    Add main function with docstring

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -21,6 +21,19 @@ def print_stats(total_size, status_codes):
 
 
 def main():
+    """
+    Main function that reads stdin line by line and computes metrics.
+
+    Args:
+        None
+
+    Returns:
+        None
+
+    Raises:
+        KeyboardInterrupt: If the user interrupts the program.
+
+    """
     total_size = 0
     status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
                     "405": 0, "500": 0}
