e07ef0e3fc6e05ca35eaef0dfcb4979fd9463756
Sirothpech <christophe.ngan@gmail.com>
2024-02-15 13:21:45 +0100

    Fix the docstrings

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""
-Write a script that reads stdin line
-by line and computes metrics:
-"""
+"""Write a script that reads stdin lineby line and computes metrics:"""
+
 import sys
 
 
