edbd10027170cd8c4c2ad3edb65f21d75faf4bf7
Sirothpech <christophe.ngan@gmail.com>
2024-02-15 13:53:13 +0100

    Fix the issues

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
 """
-Write a script that reads stdin lineby line and computes metrics
+Write a script that reads stdin line by line and computes metrics
 """
 import sys
 
+def print_stats(total_size, status_codes):
+    """
+    Prints the total file size and the count of each status code.
+    """
+    print(f"File size: {total_size}")
+    for code, count in sorted(status_codes.items()):
+        print(f"{code}: {count}")
 
 if __name__ == "__main__":
-    def print_stats(total_size, status_codes):
-        """
-        Prints the total file size and the count of each status code.
-        """
-
-        print(f"File size: {total_size}")
-        for code, count in sorted(status_codes.items()):
-            print(f"{code}: {count}")
-
     total_size = 0
     status_codes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0,
                     "405": 0, "500": 0}
