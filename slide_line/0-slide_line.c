b37a0f0bb72c35b575b4ac39e0e572437de47df3
Sirothpech <christophe.ngan@gmail.com>
2024-03-19 14:34:19 +0100

    Refactor slide_line function and remove redundant helper functions
    The slide_line function in slide_line/0-slide_line.c has been refactored to improve readability and efficiency. The previously used helper functions merge_adjacent and shift_non_zero_elements have been removed and their functionality has been integrated directly into the slide_line function. This change simplifies the codebase and reduces function call overhead.
    
    Additionally, the SLIDE_LEFT and SLIDE_RIGHT constants in slide_line/slide_line.h have been updated to use 0 and 1 respectively, instead of 1 and 2. This change aligns with common programming practices and improves code readability. The function prototypes for the removed helper functions have also been removed from this header file.

--- slide_line/0-slide_line.c
+++ slide_line/0-slide_line.c
@@ -2,90 +2,8 @@
 #include <stddef.h>
 
 /**
- * merge_adjacent - Merges adjacent elements in the array
- * @line: Array of integers containing size elements
- * @size: Number of elements in @line
- * @direction: Direction to merge the elements
- *
- * Return: void
- */
-void merge_adjacent(int *line, size_t size, int direction)
-{
-	size_t i, j;
-
-	if (direction == SLIDE_LEFT)
-	{
-		for (i = 0; i < size; ++i)
-		{
-			if (line[i] != 0)
-			{
-				for (j = i + 1; j < size && line[j] == 0; ++j)
-					;
-				if (j < size && line[i] == line[j])
-				{
-					line[i] *= 2;
-					line[j] = 0;
-					i = j;
-				}
-			}
-		}
-	}
-	else if (direction == SLIDE_RIGHT)
-	{
-		for (i = size - 1; i < size; --i)
-		{
-			if (line[i] != 0)
-			{
-				for (j = i - 1; j < size && line[j] == 0; --j)
-					;
-				if (j < size && line[i] == line[j])
-				{
-					line[i] *= 2;
-					line[j] = 0;
-					i = j;
-				}
-			}
-		}
-	}
-}
-
-/**
- * shift_non_zero_elements - Shifts non-zero elements to the left or right
- * @line: Array of integers containing size elements
- * @size: Number of elements in @line
- * @direction: Direction to slide the elements
- *
- * Return: void
- */
-void shift_non_zero_elements(int *line, size_t size, int direction)
-{
-	size_t i, j;
-
-	if (direction == SLIDE_LEFT)
-	{
-		for (i = 0, j = 0; i < size; ++i)
-		{
-			if (line[i] != 0)
-			{
-				line[j++] = line[i];
-			}
-		}
-	}
-	else if (direction == SLIDE_RIGHT)
-	{
-		for (i = size - 1, j = size - 1; i < size; --i)
-		{
-			if (line[i] != 0)
-			{
-				line[j--] = line[i];
-			}
-		}
-	}
-}
-
-/**
  * slide_line - Slides and merges an array of integers
- * towards a given direction
+ *              towards a given direction
  * @line: Array of integers containing size elements
  * @size: Number of elements in @line
  * @direction: Direction to slide the elements
@@ -94,30 +12,79 @@ void shift_non_zero_elements(int *line, size_t size, int direction)
  */
 int slide_line(int *line, size_t size, int direction)
 {
-	size_t i;
+	size_t i, j;
+	int merged = 0;
 
 	if (line == NULL || (direction != SLIDE_LEFT && direction != SLIDE_RIGHT))
 		return (0);
-
-	merge_adjacent(line, size, direction);
-	shift_non_zero_elements(line, size, direction);
 
 	if (direction == SLIDE_LEFT)
 	{
 		for (i = 0; i < size; ++i)
 		{
-			if (i >= size)
-				line[i] = 0;
+			if (line[i] == 0)
+				continue;
+			for (j = i + 1; j < size; ++j)
+			{
+				if (line[j] != 0)
+				{
+					if (line[i] == line[j] && !merged)
+					{
+						line[i] *= 2;
+						line[j] = 0;
+						merged = 1;
+					}
+					break;
+				}
+			}
+			merged = 0;
+		}
+		for (i = 0, j = 0; i < size; ++i)
+		{
+			if (line[i] != 0)
+			{
+				if (i != j)
+				{
+					line[j] = line[i];
+					line[i] = 0;
+				}
+				++j;
+			}
 		}
 	}
 	else if (direction == SLIDE_RIGHT)
 	{
-		for (i = size - 1; i < size; --i)
+		for (i = size; i > 0; --i)
 		{
-			if (i >= size)
-				line[i] = 0;
+			if (line[i - 1] == 0)
+				continue;
+			for (j = i - 1; j > 0; --j)
+			{
+				if (line[j - 1] != 0)
+				{
+					if (line[i - 1] == line[j - 1] && !merged)
+					{
+						line[i - 1] *= 2;
+						line[j - 1] = 0;
+						merged = 1;
+					}
+					break;
+				}
+			}
+			merged = 0;
+		}
+		for (i = size, j = size; i > 0; --i)
+		{
+			if (line[i - 1] != 0)
+			{
+				if (i != j)
+				{
+					line[j - 1] = line[i - 1];
+					line[i - 1] = 0;
+				}
+				--j;
+			}
 		}
 	}
-
 	return (1);
 }
--- slide_line/slide_line.h
+++ slide_line/slide_line.h
@@ -3,11 +3,9 @@
 
 #include <stddef.h>
 
-#define SLIDE_LEFT 1
-#define SLIDE_RIGHT 2
+#define SLIDE_LEFT 0
+#define SLIDE_RIGHT 1
 
-void merge_adjacent(int *line, size_t size, int direction);
 int slide_line(int *line, size_t size, int direction);
-void shift_non_zero_elements(int *line, size_t size, int direction);
 
 #endif /* SLIDE_LINE_H */
