364dec8ea43162ccebf3e711f098bbc0b5dc20c0
Sirothpech <christophe.ngan@gmail.com>
2024-05-22 17:31:43 +0200

    Refactor count_words function to use default argument for word_count

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -1,58 +1,44 @@
 #!/usr/bin/python3
-"""
-0-count
-"""
+"""print the frequencities of given words"""
 import requests
 
 
-def count_words(subreddit, word_list, after='', word_count=None):
-    """
-    Recursively queries the Reddit API, parses the titles of all hot articles,
-    and prints a sorted count of given keywords.
+def sort_key(x):
+    """ for sort """
+    return (-x[0], x[1])
 
-    :param subreddit: The subreddit to search.
-    :param word_list: A list of keywords to count.
-    :param after: The after parameter for pagination in the Reddit API.
-    :param word_count: A dictionary to keep track of keyword counts.
-    """
-    if word_count is None:
-        word_count = {word.lower(): 0 for word in word_list}
 
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'after': after, 'limit': 100}
-    response = requests.get(url, headers=headers, params=params)
-
-    if response.status_code != 200:
+def recursion(subreddit, dic, after=""):
+    """returns the title counts"""
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    p = {"after": after}
+    res = requests.get(url, headers={'User-agent': 'your bot 0.1'}, params=p,
+                       allow_redirects=False)
+    if res.status_code != 200:
+        return None
+    after = res.json().get("data").get("after")
+    for re in res.json().get("data").get("children"):
+        title = re.get("data").get("title").split(' ')
+        title_lowercase = list(map(lambda x: x.lower(), title))
+        for a in title_lowercase:
+            if a in dic.keys():
+                dic[a] += 1
+    if after is not None:
+        recursion(subreddit, dic, after)
         return
 
-    data = response.json().get('data', {})
-    posts = data.get('children', [])
 
-    for post in posts:
-        title = post['data']['title'].lower()
-        for word in word_list:
-            word_lower = word.lower()
-            word_count[word_lower] += title.split().count(word_lower)
-
-    after = data.get('after')
-    if after:
-        count_words(subreddit, word_list, after, word_count)
-    else:
-        sorted_word_count = sorted(word_count.items(),
-                                   key=lambda item: (-item[1], item[0]))
-        for word, count in sorted_word_count:
-            if count > 0:
-                print(f'{word}: {count}')
-
-
-if __name__ == "__main__":
-    import sys
-    if len(sys.argv) < 3:
-        print("Usage: {} <subreddit> <list of keywords>".format(sys.argv[0]))
-        print("Ex: {} programming 'python java javascript'"
-              .format(sys.argv[0]))
-    else:
-        subreddit = sys.argv[1]
-        word_list = [x for x in sys.argv[2].split()]
-        count_words(subreddit, word_list)
+def count_words(subreddit, word_list):
+    """prints the title counts"""
+    lower = list(map(lambda x: x.lower(), word_list))
+    li = sorted(list(set(lower)))
+    dup_d = dict(map(lambda x: [x, 0], li))
+    for i in lower:
+        dup_d[i] += 1
+    d = dict(map(lambda x: [x, 0], li))
+    c = recursion(subreddit, d)
+    dict_p = sorted(map(lambda x, y: [x, y], d.values(), d.keys()))
+    sorted_arr = sorted(dict_p, key=sort_key)
+    for k, v in sorted_arr:
+        if k != 0:
+            print('{}: {}'.format(v, k * dup_d[v]))
